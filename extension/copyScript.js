b34aaa40870d466ffeef5482e3225fdebcef4ede
Amogh Chavan <74060193+its-amogh-21@users.noreply.github.com>
2023-12-14 16:19:56 +0530

    frontend changes

---
+++ extension/copyScript.js
@@ -0,0 +1,17 @@
+// copyScript.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const outputParagraph = document.getElementById('output');
+    const copyIcon = document.getElementById('copy-icon');
+
+    copyIcon.addEventListener('click', function () {
+        const textToCopy = outputParagraph.textContent;
+        const tempTextArea = document.createElement('textarea');
+        tempTextArea.value = textToCopy;
+        document.body.appendChild(tempTextArea);
+        tempTextArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempTextArea);
+        alert('Text copied to clipboard!');
+    });
+});
